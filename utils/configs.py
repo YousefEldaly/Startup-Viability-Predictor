074f2a5c2f8f7c0ef52c78ba6836f4ccc05bfc27
“YousefEldaly” <yousefeldaly23@gmail.com>
2025-06-13 04:03:28 +0300

    concatenated scrapped data with original data

--- utils/configs.py
+++ utils/configs.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-    GOOGLEA_SEARCH_API_KEY = os.getenv('GOOGLE_SEARCH_API_KEY')
+    GOOGLE_SEARCH_API_KEY = os.getenv('GOOGLE_SEARCH_API_KEY')
     VC_SEARCH_ENGINE_ID = os.getenv('VC_SEARCH_ENGINE_ID')
     LLM_MODEL = os.getenv('LLM_MODEL')
     LOG_FILE_NAME = os.getenv('LOG_FILE_NAME')
